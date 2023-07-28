d451c8fcdf68c515b8d6b9de3eaf9381e9abe61c
Fekensa Degefa <fekebilo@gmail.com>
2023-07-28 10:21:24 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,40 @@
+    var abt = document.getElementById("About");
+    var exp = document.getElementById("Experience");
+    var edu = document.getElementById("Education");
+    var trn = document.getElementById("Training");
+    var crt = document.getElementById("Certification");
+    function openAbout() {
+        abt.style.display = "block"; 
+        exp.style.display = "none"; 
+        edu.style.display = "none"; 
+        trn.style.display = "none"; 
+        crt.style.display = "none";              
+    }  
+    function openExp() {
+        abt.style.display = "none"; 
+        exp.style.display = "block"; 
+        edu.style.display = "none"; 
+        trn.style.display = "none"; 
+        crt.style.display = "none";              
+    }       
+    function openEdu() {
+        abt.style.display = "none"; 
+        exp.style.display = "none"; 
+        edu.style.display = "block"; 
+        trn.style.display = "none"; 
+        crt.style.display = "none";              
+    }       
+    function openTrn() {
+        abt.style.display = "none"; 
+        exp.style.display = "none"; 
+        edu.style.display = "none"; 
+        trn.style.display = "block"; 
+        crt.style.display = "none";              
+    }            
+    function openCrt() {
+        abt.style.display = "none"; 
+        exp.style.display = "none"; 
+        edu.style.display = "none"; 
+        trn.style.display = "none"; 
+        crt.style.display = "block";              
+    }       
